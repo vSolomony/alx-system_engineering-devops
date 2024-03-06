0de13c15fc53eb9007e29dbc1cf6cc5792b2c31c
vSolomony <mohamed.a.kassem95@gmail.com>
2024-03-05 18:40:08 -0800

    123

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,13 +3,20 @@
 
 import requests
 
-
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    data = response.json()
-    if response.status_code != 200:
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        data = response.json()
+        
+        if response.status_code != 200:
+            return 0
+        else:
+            return data['data']['subscribers']
+    
+    except Exception as e:
+        print(f"Error: {e}")
         return 0
-    else:
-        return data['data']['subscribers']
+
